af12096ce52faa2056e4dfd7978f864dcb6af702
David Proctor <davidproctorsemail@gmail.com>
2019-02-16 17:00:33 -0800

    Convert to HTML

--- invest.php
+++
@@ -1,7 +0,0 @@
-		<h2>
-			Under construction. Get rich soon
-		</h2>
-		
-		<center>
-			<img src="construction1.gif" alt="It means that the page is under construction" title="It means that the page is under construction" />
-		</center>
